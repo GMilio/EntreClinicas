ff35219d033c1bf1e4734ef025e62b05c5103d14
Guilherme Mariano <guilherme.milioranca@gcom.com.br>
2022-05-10 22:24:07 -0300

    header

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -7,18 +7,17 @@ import Footer from '../src/components/Containers/Footer/footer'
 
 export default function Home() {
   return (
+
     <div className="container">
       <Head>
         <link rel="styleeshet" href="./style.css"></link>
         <title>Entre Clinicas</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
       <main>
     <Header></Header>
       </main>
-
-
 
       <Footer></Footer>
 

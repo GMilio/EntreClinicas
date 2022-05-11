4d9a5d4ab281ea55f9fca2a87713ffaba017987e
Leonrse <lrseleoterio@gmail.com>
2022-05-10 23:17:55 -0300

    zerobotao

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,5 +1,6 @@
 import '../Styles/style.css'
 
+if (typeof document === 'undefined') { // @ts-ignore global.document = { querySelector: function () {}, }; }
 // This default export is required in a new pages/_app.js file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
